b83573f574505973c290ca561a51da134785711f
skybye02 <45346929+skybye02@users.noreply.github.com>
2019-07-12 09:55:43 +0530

    Update README.js

--- README.js
+++ README.js
@@ -232,3 +232,5 @@ https://medium.com/@alexmngn/from-reactjs-to-react-native-what-are-the-main-diff
 // https://stackoverflow.com/questions/48657481/in-javascript-is-constructor-mandatory-in-a-class
 // sanction location section
 // https://mongoosejs.com/docs/geojson.html
+
+// https://docs.mongodb.com/manual/reference/operator/aggregation/geoNear/#pipe._S_geoNear
