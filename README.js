96dd196ee271f0a35b9345816ce27c7411c69144
skybye02 <45346929+skybye02@users.noreply.github.com>
2019-07-24 18:33:08 +0530

    Update README.js

--- README.js
+++ README.js
@@ -468,3 +468,5 @@ try {
 // dont take it as offence ...I like to spend my time in aloneMode..because I love this mode 
 // its nothing that I dont like my friends or colloeague or whatever..if u need help any time 
 // if I can help with my power then I will definetly execute help 24*7..
+// so if I am not coming in some gathering or party or whatever dont tag me as antisocial
+// enjoyyy..i will be with you when all motherfucker will not with you....
