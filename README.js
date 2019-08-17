4cbd0fea271f3123db3d7cdf401381deccc23de7
skybye02 <45346929+skybye02@users.noreply.github.com>
2019-08-17 09:50:43 +0530

    Update README.js

--- README.js
+++ README.js
@@ -628,8 +628,8 @@ sweet:"daisy milk'
 // https://stackoverflow.com/questions/38093760/how-to-access-a-dom-element-in-react-what-is-the-equilvalent-of-document-getele
 // https://hackernoon.com/virtual-dom-in-reactjs-43a3fdb1d130
 // from eloquent javascript book 
-console.log("Litti".repeat(3));
-//output: LittiLittiLitti (awesome 00)
+console.log("LittiL".repeat(3));
+//output: LittiLLittiLLittiL (awesome 00)
 
 //explanation :A string can be repeated with the repeat method, which creates a new string
 //containing multiple copies of the original string, glued together.
