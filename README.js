dac58c3e1cea2a16dd85ef9ae3cba7a4de178fc5
skybye02 <45346929+skybye02@users.noreply.github.com>
2019-08-19 09:12:16 +0530

    Update README.js

--- README.js
+++ README.js
@@ -726,3 +726,4 @@ refer past screenshot to achieve the same
 // the sweetVegetable delicious feel
 // for performing self ohh yeahh you need to have strong visualization skill
 // if u dont have then you are pseudo human dna
+// http://programmerblog.net/nodejs-user-registration-tutorial/
