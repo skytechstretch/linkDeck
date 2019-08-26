960bc7c9337aa3a3c278f6f035f71139c37572da
skybye02 <45346929+skybye02@users.noreply.github.com>
2019-08-26 12:28:51 +0530

    Update README.js

--- README.js
+++ README.js
@@ -870,5 +870,5 @@ refer past screenshot to achieve the same
 // I am psychologist expertt..but this time 
 // i need some clear hintt
 
-// nice voice sHen is lucky noun
+// nice voice sHen 🐔 is lucky noun
 // https://youtu.be/aSZVYZTze74
